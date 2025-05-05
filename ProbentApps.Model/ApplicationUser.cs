ffd77e38f6be5a34ff74439681f3cd4b04b1a227
Thibaut Henriette--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-05-05 16:08:56 +0200

    refactor(main): Use a constant for root user id and optimize database seeding logic

--- ProbentApps.Model/ApplicationUser.cs
+++ ProbentApps.Model/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace ProbentApps.Model;
 [Table("Users", Schema = "identity")]
 public class ApplicationUser : IdentityUser<Guid>, IHashedNormalizationUser
 {
+    public static readonly Guid RootId = new(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
     [Column(TypeName = "binary(64)")]
     public byte[]? NormalizationSalt { get; set; }
 }
