be5260b07ab662edd678ba1f5ed776305b398e2d
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-05-28 18:03:07 +0200

    feat(data-services): Add model data management services based on main application usage scenarios

---
+++ ProbentApps.Services/Data/IInvoicingManager.cs
@@ -0,0 +1,20 @@
+﻿using ProbentApps.Model;
+
+namespace ProbentApps.Services.Data;
+
+public interface IInvoicingManager : IRepository<Invoice>
+{
+    Task<InvoiceDraftingResult> DraftInvoiceAsync(InvoiceData invoice, CancellationToken cancellationToken = default);
+
+    Task<InvoiceDraftModificationResult> ModifyInvoiceDraftAsync(Guid invoiceId, InvoiceData invoiceData, CancellationToken cancellationToken = default);
+
+    Task<InvoiceDeletionResult> DeleteInvoiceAsync(Guid invoiceId, CancellationToken cancellationToken = default);
+
+    Task<AdvancementInvoicingResult> InvoiceAdvancementsAsync(Guid invoiceId, IEnumerable<Guid> advancementIds, CancellationToken cancellationToken = default);
+
+    Task<AdvancementRemovalFromInvoiceResult> RemoveAdvancementFromInvoiceAsync(Guid invoiceId, Guid advancementId, CancellationToken cancellationToken = default);
+
+    Task<InvoiceRequestResult> RequestInvoiceAsync(Guid invoiceId, CancellationToken cancellationToken = default);
+
+    Task<InvoiceSubmissionResult> SubmitInvoiceAsync(Guid invoiceId, string invoiceNumber, CancellationToken cancellationToken = default);
+}
---
+++ ProbentApps.Services/Data/InvoiceData.cs
@@ -0,0 +1,87 @@
+﻿using ProbentApps.Model;
+
+namespace ProbentApps.Services.Data;
+
+public record struct InvoiceData(string Name);
+
+public record struct InvoiceDraftingResult(InvoiceDraftingResult.Status Result, Invoice? Entity = null)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        InvalidData
+    }
+}
+
+public record struct InvoiceDraftModificationResult(InvoiceDraftModificationResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        InvalidData
+    }
+}
+
+public record struct AdvancementInvoicingResult(AdvancementInvoicingResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        AdvancementNotFound,
+        InvoiceNotFound,
+        InvoiceAlreadyRequested
+    }
+}
+
+public record struct AdvancementRemovalFromInvoiceResult(AdvancementRemovalFromInvoiceResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        AdvancementNotFound,
+        AdvancementNotInvoicedByInvoice,
+        InvoiceNotFound,
+        InvoiceAlreadyRequested
+    }
+}
+
+public record struct InvoiceDeletionResult(InvoiceDeletionResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        NotFound,
+        AlreadyRequested
+    }
+}
+
+public record struct InvoiceRequestResult(InvoiceRequestResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        NotFound,
+        InvoiceEmpty,
+        AlreadyRequested
+    }
+}
+
+public record struct InvoiceSubmissionResult(InvoiceSubmissionResult.Status Result)
+{
+    public enum Status : byte
+    {
+        Success,
+        Cancelled,
+        NotFound,
+        NotYetRequested,
+        UnregisteredClient,
+        AlreadySubmitted,
+        InvalidData
+    }
+}
