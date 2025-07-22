8f40d46cefb14327b2b1dde4a8f8232fb92c345e
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-07-22 10:59:24 +0200

    feat(reverse-proxy): Add environment-specific configuration management infrastructure

---
+++ ProbentApps.Configuration/IConfigurationManagerExtensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Configuration;
+
+namespace ProbentApps.Configuration;
+
+public static class IConfigurationManagerExtensions
+{
+    public static void AddProductionConfiguration(this IConfigurationManager configuration)
+    {
+    }
+
+    public static void AddDevelopmentConfiguration(this IConfigurationManager configuration)
+    {
+    }
+}
