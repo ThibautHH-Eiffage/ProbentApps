899afe1385bb9e5772e1db8f1334311dd88c236a
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-08-05 10:43:25 +0200

    feat(ui): Add affair visualization components and logic

--- ProbentApps.Model/Affair.cs
+++ ProbentApps.Model/Affair.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace ProbentApps.Model;
 
 [Table("Affairs")]
-public class Affair : Structure
+public class Affair : Structure, IEquatable<Affair>
 {
     public bool IsArchived { get; set; }
 
@@ -12,4 +12,15 @@ public class Affair : Structure
     public required Client Client { get; set; }
 
     public required IList<Order> Orders { get; set; }
+
+    public bool Equals(Affair? other) => other is not null
+        && (Id.Equals(other.Id)
+            || (Code?.Equals(other?.Code, StringComparison.Ordinal)
+                ?? Name.Equals(other?.Name, StringComparison.CurrentCultureIgnoreCase)));
+
+    public override bool Equals(object? obj) => Equals(obj as Affair);
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public override string ToString() => Name;
 }
