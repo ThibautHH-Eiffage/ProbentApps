6ea72d42a4952039544d34990437b97903a3de88
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-08-06 11:33:05 +0200

    refactor(reverse-proxy): Change forwarded headers options handling and defaults

--- ProbentApps.Services/Options/ForwardedHeadersOptionsConfiguration.cs
+++ ProbentApps.Services/Options/ForwardedHeadersOptionsConfiguration.cs
@@ -1,21 +1,44 @@
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HostFiltering;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using ProbentApps.Configuration;
 
 namespace ProbentApps.Services.Options;
 
 internal class ForwardedHeadersOptionsConfiguration(IOptions<HostFilteringOptions> hostFilteringOptions, IConfiguration configuration)
-    : NamedConfigureFromConfigurationOptions<ForwardedHeadersOptions>(null, configuration.GetSection(nameof(ForwardedHeaders)))
+    : IConfigureOptions<ForwardedHeadersOptions>
 {
-    public override void Configure(string? name, ForwardedHeadersOptions options)
+    public void Configure(ForwardedHeadersOptions options)
     {
-        base.Configure(name, options);
+        var forwardedHeaders = configuration.GetRequiredSection(nameof(ForwardedHeaders));
 
-        if (options.AllowedHosts is not null && options.AllowedHosts.Any())
-            return;
+        foreach (string host in forwardedHeaders[nameof(options.AllowedHosts)]?
+            .Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            ?? [])
+            options.AllowedHosts.Add(host);
 
-        options.AllowedHosts = hostFilteringOptions.Value.AllowedHosts;
+        if (!options.AllowedHosts.Any())
+            options.AllowedHosts = hostFilteringOptions.Value.AllowedHosts;
+
+        bool cleared = false;
+        foreach (string ip in forwardedHeaders[nameof(options.KnownProxies)]?
+            .Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            ?? [])
+        {
+            if (!cleared)
+            {
+                cleared = true;
+                options.KnownProxies.Clear();
+            }
+            options.KnownProxies.Add(IPAddress.Parse(ip));
+        }
+
+        if (configuration.IsReverseProxyingEnabled())
+            options.ForwardedHeaders = ForwardedHeaders.All & ~ForwardedHeaders.XForwardedPrefix;
+        options.KnownNetworks.Clear();
+        options.RequireHeaderSymmetry = true;
     }
 }
