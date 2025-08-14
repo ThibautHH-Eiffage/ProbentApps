b68e268a970cf7083340c0c7a01462731e4cf509
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-08-14 11:15:17 +0200

    perf(data-services): Prune unused data columns from affair query

--- ProbentApps.Services.Data/AffairRepository.cs
+++ ProbentApps.Services.Data/AffairRepository.cs
@@ -17,6 +17,37 @@ public class AffairRepository(ApplicationDbContext context, UserManager<Applicat
                 .ThenInclude(a => a.Invoice)
         .WhereStructureIsAdministeredBy(context.Structures, Guid.Parse(userManager.GetUserId(user)!), user.GetExtraManagedStructures())
         .Where(a => a.IsArchived == archived)
+        .Select(static a => new Affair
+        {
+            Id = a.Id,
+            Name = a.Name,
+            Code = a.Code,
+            ShortCode = a.ShortCode,
+            Client = new Client
+            {
+                Id = a.Client.Id,
+                Name = a.Client.Name
+            },
+            IsArchived = a.IsArchived,
+            Manager = EF.Property<Guid?>(a, "ManagerId") == null ? null : new ApplicationUser
+            {
+                Id = a.Manager!.Id,
+                UserName = a.Manager.UserName,
+                Email = a.Manager.Email
+            },
+            Orders = a.Orders.Select(o => new Order
+            {
+                Id = o.Id,
+                Name = o.Name,
+                TotalPrice = o.TotalPrice,
+                Advancements = o.Advancements.Select(a => new Advancement
+                {
+                    Id = a.Id,
+                    Value = a.Value,
+                    Invoice = EF.Property<Guid?>(a, "InvoiceId") == null ? null : new Invoice { Id = a.Invoice!.Id }
+                }).ToList()
+            }).ToList()
+        })
         .AsAsyncEnumerable();
 }
 
