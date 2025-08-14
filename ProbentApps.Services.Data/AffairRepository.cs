978aa62bcfa9f2f4f247d76b8f216e17ea19ffc0
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-08-14 09:07:04 +0200

    refactor(data-services): Add IQueryable extension for structure access control

--- ProbentApps.Services.Data/AffairRepository.cs
+++ ProbentApps.Services.Data/AffairRepository.cs
@@ -1,34 +1,30 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProbentApps.Model;
 using ProbentApps.Services.Data.Abstractions;
 using ProbentApps.Services.Database.Abstractions.Contexts;
-using System.Security.Claims;
 
 namespace ProbentApps.Services.Data;
 
 public class AffairRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager) : DefaultRepository<Affair>(context), IAffairRepository
 {
-    private Func<ApplicationDbContext, Guid, Guid[], bool, IAsyncEnumerable<Affair>>? _getAffairsFor;
-    private Func<ApplicationDbContext, Guid, Guid[], bool, IAsyncEnumerable<Affair>> GetAffairsFor => _getAffairsFor ??=
-        EF.CompileAsyncQuery((ApplicationDbContext context, Guid userId, Guid[] extraAffairIds, bool archived) => context.Affairs
-            .Include(a => a.Client)
-            .Include(a => a.Orders)
-            .ThenInclude(o => o.Advancements)
-            .ThenInclude(a => a.Invoice)
-            .Where(a => a.IsArchived == archived
-                && (extraAffairIds.Contains(a.Id) || context.Structures
-                    .Where(s => s.Manager!.Id == userId)
-                    .Any(s => a.Code.StartsWith(s.Code)))));
-
     private Func<ApplicationDbContext, Guid, IAsyncEnumerable<Affair>>? _getAllAffairsFrom;
     private Func<ApplicationDbContext, Guid, IAsyncEnumerable<Affair>> GetAllAffairsFrom => _getAllAffairsFrom ??=
         EF.CompileAsyncQuery((ApplicationDbContext context, Guid structureId) => context.Affairs
             .Where(a => a.Code.StartsWith(context.Structures.First(s => s.Id == structureId).Code)));
 
-    IAsyncEnumerable<Affair> IAffairRepository.GetAffairsFor(ClaimsPrincipal user, bool archived) =>
-        GetAffairsFor(Context, Guid.Parse(userManager.GetUserId(user)!), user.GetExtraManagedAffairs(), archived);
+    IAsyncEnumerable<Affair> IAffairRepository.GetAffairsFor(ClaimsPrincipal user, bool archived) => Context.Affairs
+        .AsNoTrackingWithIdentityResolution()
+        .Include(a => a.Client)
+        .Include(a => a.Orders)
+            .ThenInclude(o => o.Advancements)
+                .ThenInclude(a => a.Invoice)
+        .WhereStructureIsAdministeredBy(context.Structures, Guid.Parse(userManager.GetUserId(user)!), user.GetExtraManagedStructures())
+        .Where(a => a.IsArchived == archived)
+        .AsAsyncEnumerable();
 
     IAsyncEnumerable<Affair> IAffairRepository.GetAllAffairsFrom(Guid structureId) =>
         GetAllAffairsFrom(Context, structureId);
 }
+
