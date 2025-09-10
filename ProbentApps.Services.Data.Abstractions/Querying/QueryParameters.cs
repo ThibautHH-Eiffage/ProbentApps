011e74ffb48b011132d3c6dfddc295abfdcd5c07
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-09-10 19:07:06 +0200

    feat(data-services): Add flexible and generic API for querying table data from entity repositories

--- ProbentApps.Services.Data.Abstractions/Querying/QueryParameters.cs
+++ ProbentApps.Services.Data.Abstractions/Querying/QueryParameters.cs
@@ -4,6 +4,7 @@ namespace ProbentApps.Services.Data.Abstractions.Querying;
 
 public record struct QueryParameters<T>(
     Func<IQueryable<T>, IQueryable<T>> Filter,
+    Func<IQueryable<T>, IQueryable<T>>? Select,
     Func<IQueryable<T>, IQueryable<T>> SortAndPaginate,
     ClaimsPrincipal User)
     where T : class;
--- ProbentApps.Services.Data/DefaultRepository.cs
+++ ProbentApps.Services.Data/DefaultRepository.cs
@@ -1,5 +1,11 @@
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using ProbentApps.Model;
 using ProbentApps.Services.Data.Abstractions;
+using ProbentApps.Services.Data.Abstractions.Querying;
 using ProbentApps.Services.Database.Abstractions.Contexts;
 
 namespace ProbentApps.Services.Data;
@@ -7,10 +13,37 @@ namespace ProbentApps.Services.Data;
 public class DefaultRepository<T>(ApplicationDbContext context) : IRepository<T>
     where T : class, IEntity
 {
+    private static readonly MethodInfo QueryParameterMarkerMethodInfo = typeof(EF).GetTypeInfo()
+        .GetDeclaredMethod(nameof(EF.Parameter))
+        !.MakeGenericMethod(typeof(Guid));
+
+    private static readonly PropertyInfo EntityIdPropertyInfo = typeof(IEntity).GetTypeInfo()
+        .GetDeclaredProperty(nameof(IEntity.Id))!;
+
     protected ApplicationDbContext Context { get; } = context;
+
+    protected virtual IQueryable<T> ApplyIdentityFilter(IQueryable<T> query, ClaimsPrincipal user) => query;
+
+    protected virtual IQueryable<T> ApplyDefaultDataSelection(IQueryable<T> query) => query;
 
     ValueTask<T?> IRepository<T>.FindAsync(Guid id, CancellationToken cancellationToken) =>
         Context.Set<T>().FindAsync([id], cancellationToken);
 
-    IQueryable<T> IRepository<T>.Query() => Context.Set<T>();
+    async Task<(IEnumerable<T> data, int count)> IRepository<T>.GetTableDataForAsync(QueryParameters<T> parameters, CancellationToken cancellationToken)
+    {
+        var query = (parameters.Select ?? ApplyDefaultDataSelection)(parameters.Filter(ApplyIdentityFilter(Context.Set<T>().AsNoTrackingWithIdentityResolution(), parameters.User)));
+
+        return (await parameters.SortAndPaginate(query).ToArrayAsync(cancellationToken), await query.CountAsync(cancellationToken));
+    }
+
+    IQueryable<T> IRepository<T>.ApplyEntityFilter(IQueryable<T> query, IEntity entity, LambdaExpression targetEntityExpression) => query
+        .Where(Expression.Lambda<Func<T, bool>>(
+            Expression.Equal(
+                targetEntityExpression.Parameters.Single()
+                    .MakeMemberAccess(targetEntityExpression.GetPropertyAccess())
+                    .MakeMemberAccess(EntityIdPropertyInfo),
+                Expression.Call(null, QueryParameterMarkerMethodInfo, Expression.Constant(entity.Id))
+            ),
+            targetEntityExpression.Parameters
+        ));
 }
