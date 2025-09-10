a554151e0fb67409ca18366d1dd7667f60bb6630
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-09-10 22:25:41 +0200

    feat(data-services): Implement table data querying in AffairRepository and remove previous API

--- ProbentApps.Services.Data.Abstractions/IAffairRepository.cs
+++
@@ -1,9 +0,0 @@
-using ProbentApps.Model;
-using System.Security.Claims;
-
-namespace ProbentApps.Services.Data.Abstractions;
-
-public interface IAffairRepository : IRepository<Affair>
-{
-    IAsyncEnumerable<Affair> GetAffairsFor(ClaimsPrincipal user, bool archived);
-}
--- ProbentApps.Services.Data/AffairRepository.cs
+++ ProbentApps.Services.Data/AffairRepository.cs
@@ -3,25 +3,23 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProbentApps.Model;
 using ProbentApps.Services.Data.Abstractions;
+using ProbentApps.Services.Data.Abstractions.Querying;
 using ProbentApps.Services.Database.Abstractions.Contexts;
 
 namespace ProbentApps.Services.Data;
 
-public class AffairRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager) : DefaultRepository<Affair>(context), IAffairRepository
+public class AffairRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager) : DefaultRepository<Affair>(context)
 {
-    IAsyncEnumerable<Affair> IAffairRepository.GetAffairsFor(ClaimsPrincipal user, bool archived) => Context.Affairs
-        .AsNoTrackingWithIdentityResolution()
-        .Include(a => a.Client)
-        .Include(a => a.Orders)
-            .ThenInclude(o => o.Advancements)
-                .ThenInclude(a => a.Invoice)
-        .WhereStructureIsAdministeredBy(context.Structures, Guid.Parse(userManager.GetUserId(user)!), user.GetExtraManagedStructures())
-        .Where(a => a.IsArchived == archived)
+    protected override IQueryable<Affair> ApplyIdentityFilter(IQueryable<Affair> query, ClaimsPrincipal user) => query
+        .WhereStructureIsAdministeredBy(Context.Structures,
+            Guid.Parse(userManager.GetUserId(user)!),
+            user.GetExtraManagedStructures());
+
+    protected override IQueryable<Affair> ApplyDefaultDataSelection(IQueryable<Affair> query) => query
         .Select(static a => new Affair
         {
             Id = a.Id,
             Name = a.Name,
-            Code = a.Code,
             ShortCode = a.ShortCode,
             Client = new Client
             {
@@ -29,25 +27,12 @@ public class AffairRepository(ApplicationDbContext context, UserManager<Applicat
                 Name = a.Client.Name
             },
             IsArchived = a.IsArchived,
-            Manager = EF.Property<Guid?>(a, "ManagerId") == null ? null : new ApplicationUser
-            {
-                Id = a.Manager!.Id,
-                UserName = a.Manager.UserName,
-                Email = a.Manager.Email
-            },
             Orders = a.Orders.Select(o => new Order
             {
                 Id = o.Id,
                 Name = o.Name,
                 TotalPrice = o.TotalPrice,
-                Advancements = o.Advancements.Select(a => new Advancement
-                {
-                    Id = a.Id,
-                    Value = a.Value,
-                    Invoice = EF.Property<Guid?>(a, "InvoiceId") == null ? null : new Invoice { Id = a.Invoice!.Id }
-                }).ToList()
             }).ToList()
-        })
-        .AsAsyncEnumerable();
+        });
 }
 
