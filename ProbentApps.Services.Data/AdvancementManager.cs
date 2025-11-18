9ffc2b8b5b2be4cf0b00e3eb0d40df01aebfb277
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-11-18 13:09:23 +0100

    refactor(data-services): Update AdvancementManager to make use of DefaultRepository's query infrastructure

--- ProbentApps.Services.Data/AdvancementManager.cs
+++ ProbentApps.Services.Data/AdvancementManager.cs
@@ -1,24 +1,35 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProbentApps.Model;
 using ProbentApps.Services.Data.Abstractions;
 using ProbentApps.Services.Database.Abstractions.Contexts;
-using ProbentApps.Model;
 
 namespace ProbentApps.Services.Data;
 
 internal class AdvancementManager(IDbContextFactory<ApplicationDbContext> contextFactory,
+    UserManager<ApplicationUser> userManager,
     TimeProvider timeProvider,
     ILogger<AdvancementManager> logger)
     : DefaultRepository<Advancement>(contextFactory), IAdvancementManager
 {
-    private Func<ApplicationDbContext, Guid, CancellationToken, Task<Advancement?>>? _getAdvancement;
-    private Func<ApplicationDbContext, Guid, CancellationToken, Task<Advancement?>> GetAdvancement => _getAdvancement ??=
-        EF.CompileAsyncQuery((ApplicationDbContext context, Guid id, CancellationToken _) => context.Advancements
-            .Include(static a => a.Report)
-            .Include(static a => a.Invoice)
-            .FirstOrDefault(a => a.Id == id));
+    protected override IQueryable<Advancement> ApplyIdentityFilter(IQueryable<Advancement> query, ClaimsPrincipal user) => query
+        .WhereStructureIsAdministeredBy(Guid.Parse(userManager.GetUserId(user)!),
+            user.GetExtraManagedStructures(),
+            Context.Structures,
+            static a => a.Order.Affair);
+
+    protected override IQueryable<Advancement> ApplyDefaultDataSelection(IQueryable<Advancement> query) => query
+        .Include(static a => a.Order)
+        .ThenInclude(static o => o.Advancements)
+        .Include(static a => a.Report)
+        .Include(static a => a.Invoice);
 
     async Task<AdvancementCreationResult> IAdvancementManager.CreateAdvancementAsync(AdvancementCreationData data, CancellationToken cancellationToken)
     {
+        await using var scope = MakeQueryScope();
+
         if (await Context.Orders.FindAsync([data.OrderId], cancellationToken) is not Order order)
         {
             return new AdvancementCreationResult(AdvancementCreationResult.Status.OrderNotFound);
@@ -51,8 +62,18 @@ internal class AdvancementManager(IDbContextFactory<ApplicationDbContext> contex
 
     async Task<AdvancementDeletionResult> IAdvancementManager.DeleteAdvancementAsync(Guid advancementId, CancellationToken cancellationToken)
     {
-        if (await GetAdvancement(Context, advancementId, cancellationToken) is not Advancement advancement)
+        await using var scope = MakeQueryScope();
+
+        if (await Context.Advancements.Select(static a => new Advancement
+                {
+                    Id = a.Id,
+                    Report = a.Report == null ? null : new Report { AcceptanceDate = a.Report.AcceptanceDate },
+                    Invoice = a.Invoice == null ? null : new Invoice { RequestDate = a.Invoice.RequestDate }
+                })
+                .FirstOrDefaultAsync(a => a.Id == advancementId, cancellationToken) is not Advancement advancement)
+        {
             return new AdvancementDeletionResult(AdvancementDeletionResult.Status.NotFound);
+        }
 
         if (advancement.Report?.AcceptanceDate is not null)
         {
@@ -74,8 +95,22 @@ internal class AdvancementManager(IDbContextFactory<ApplicationDbContext> contex
 
     async Task<AdvancementUpdateResult> IAdvancementManager.UpdateAdvancementAsync(Guid advancementId, AdvancementUpdateData data, CancellationToken cancellationToken)
     {
-        if (await GetAdvancement(Context, advancementId, cancellationToken) is not Advancement advancement)
+        await using var scope = MakeQueryScope();
+
+        if (await Context.Advancements.Select(static a => new Advancement
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    Date = a.Date,
+                    Description = a.Description,
+                    Value = a.Value,
+                    Report = a.Report == null ? null : new Report { AcceptanceDate = a.Report.AcceptanceDate },
+                    Invoice = a.Invoice == null ? null : new Invoice { RequestDate = a.Invoice.RequestDate }
+                })
+                .FirstOrDefaultAsync(a => a.Id == advancementId, cancellationToken) is not Advancement advancement)
+        {
             return new AdvancementUpdateResult(AdvancementUpdateResult.Status.NotFound);
+        }
 
         if (advancement.Report?.AcceptanceDate is not null)
         {
