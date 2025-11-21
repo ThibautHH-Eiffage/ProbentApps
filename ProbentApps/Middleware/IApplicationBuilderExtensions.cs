3afe1ad3ce024905f91a7d689c69e1d3979b212f
Thibaut Hebert--Henriette <Thibaut.HEBERT-HENRIETTE@eiffage.com>
2025-11-21 09:17:02 +0100

    fix(ui): Add missing declaration of support of the France's french CultureInfo

--- ProbentApps/Middleware/IApplicationBuilderExtensions.cs
+++ ProbentApps/Middleware/IApplicationBuilderExtensions.cs
@@ -19,5 +19,5 @@ public static class IApplicationBuilderExtensions
         .UseAuthentication()
         .UseAuthorization()
         .UseAntiforgery()
-        .UseRequestLocalization("en", "fr");
+        .UseRequestLocalization("en", "fr", "fr-FR");
 }
