1aaa343e55882ebdf6d90e527705ea04eb7cfcc8
Yves HEBERT <yves.hebert@probent.com>
2025-12-09 17:23:31 +0100

    feat(structures): Enregistrement des services de données par défaut pour les utilisateurs et les types de structure

--- ProbentApps.Services.Data/IServiceCollectionExtensions.cs
+++ ProbentApps.Services.Data/IServiceCollectionExtensions.cs
@@ -7,6 +7,8 @@ namespace ProbentApps.Services.Data;
 public static class IServiceCollectionExtensions
 {
     public static IServiceCollection AddDataServices(this IServiceCollection services) => services
+        .AddScoped<IRepository<ApplicationUser>, DefaultRepository<ApplicationUser>>()
+        .AddScoped<IRepository<StructureType>, DefaultRepository<StructureType>>()
         .AddScoped<IRepository<Affair>, AffairRepository>()
         .AddScoped<IStructureManager, StructureManager>()
         .AddScoped<IRepository<Structure>, StructureManager>()
